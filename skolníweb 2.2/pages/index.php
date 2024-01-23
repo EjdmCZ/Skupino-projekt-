8499d19b6e75212f78764ccb1872e223039f2103
Stepan-Sestak <117169773+Stepan-Sestak@users.noreply.github.com>
2024-01-23 13:14:26 +0100

    Update index.php

--- skolníweb 2.2/pages/index.php
+++ skolníweb 2.2/pages/index.php
@@ -1,11 +1,5 @@
 <?php 
   session_start();
-   if (!isset($_SESSION["prihlaseni"])) {
-       $_SESSION["prihlaseni"] = false;
-      }
-  if (!isset($_SESSION["username"])) {
-       $_SESSION["username"] = "Nepřihlášen" ;
-      }
  ?>
 <!DOCTYPE html>
 <html lang="en">
