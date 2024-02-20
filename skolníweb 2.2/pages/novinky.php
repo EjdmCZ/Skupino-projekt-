9106e568baf8b8569722dadfd1fe7fb3efc15a18
Stepan-Sestak <117169773+Stepan-Sestak@users.noreply.github.com>
2024-02-20 13:21:24 +0100

    Update novinky.php

--- skolníweb 2.2/pages/novinky.php
+++ skolníweb 2.2/pages/novinky.php
@@ -10,7 +10,7 @@
   <body>
     <div class="nav">
       <ul>
-        <li><a class="Novinky" href="novinky.html">Novinky</a></li>
+        <li><a class="Novinky" href="novinky.php">Novinky</a></li>
         <li><a class="Domov" href="index.php">Domů</a></li>
         <li><a class="Hry" href="hry.html">Hry</a></li>
         <li><a class="onás" href="onas.html">O nás</a></li>
@@ -23,6 +23,18 @@
       </ul>
       <script src="../javascript/js.js "></script>
     </div>
+
+    <?php
+
+      $novinka = array(
+        "Jméno" => "Last Holiday" ,
+        "Vydaní" => "18. května 2023 ",
+        "Vydavatel" => "Boomer Games",
+        "Popis" => "Hra nebyla plně vydaná, a její budoucnost je nejsitá. Hra je v Předběžným přístupu vyšla ve strašném stavu, a plného vydání se nejspíš nedočká"
+         );
+
+    ?>
+
     <table>
       <tr class="ramecek">
         <th>Last Holiday</th>
@@ -37,6 +49,29 @@
           někdy na přelomu let 2023 a 2024 by se měla dočkat plného vydání.
         </th>
       </tr>
+
+      <table class="ramecek">
+        <tr>
+        <th>
+          <?php echo $novinka["Jméno"] ?>
+        </th>
+        </tr>
+        <tr>
+        <th>
+          <?php echo $novinka["Vydaní"] ?>
+        </th>
+        </tr>
+        <tr>
+        <th>
+          <?php echo $novinka["Vydavatel"] ?> 
+        </th>
+          </tr>
+        <tr>  
+        <th>
+          <?php echo $novinka["Popis"] ?>
+        </th>
+        </tr>
+      </table>
     </table>
   </body>
 </html>
