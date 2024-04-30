bf486b353850bf0a490a43bc4c467db6584fca3b
Lusiaccz <117169491+Lusiaccz@users.noreply.github.com>
2024-04-30 12:49:15 +0200

    Add files via upload

---
+++ skolníweb 2.2/pages/custom.php
@@ -0,0 +1,37 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["barva"])) {
+    $vybrana_barva = $_POST["barva"];
+    setcookie("vybrana_barva", $vybrana_barva, time() + (86400 * 30), "/");
+    header("Location: custom.php");
+}
+function nacti_vybranou_barvu() {
+    if (isset($_COOKIE["vybrana_barva"])) {
+        return $_COOKIE["vybrana_barva"];
+    } else {
+        return "#ccc";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Výběr barvy stránky</title>
+    <style>
+        body {
+            background-color: <?php echo nacti_vybranou_barvu(); ?>;
+            }
+    </style>
+</head>
+<body>
+
+<h2 ><a href=index.php>Výběr barvy stránky</a></h2>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <label>Vyberte barvu:</label><br>
+    <input type="color" name="barva"><br><br>
+    <input type="submit" value="Uložit">
+</form>
+
+</body>
+</html>
