48c7a87fffe0c5f6ccfb5f9107efcfef2c631200
Stepan-Sestak <117169773+Stepan-Sestak@users.noreply.github.com>
2024-05-07 14:10:42 +0200

    Update novinky.php

--- skolníweb 2.2/pages/novinky.php
+++ skolníweb 2.2/pages/novinky.php
@@ -38,7 +38,7 @@ function nacti_vybranou_barvu() {
   <body>
   <?php include 'navbar.php';?>
 
-    <table>
+     <table>
       <tr class="ramecek">
         <th>Last Holiday</th>
         <th>
@@ -52,7 +52,29 @@ function nacti_vybranou_barvu() {
           někdy na přelomu let 2023 a 2024 by se měla dočkat plného vydání.
         </th>
       </tr>
+      </table>
+      <table>
+        <tr class="ramecek">
+        <th>GTA VI</th>
+        <th>
+          <img class="gta"
+            src="https://cdn.alza.cz/Foto/ImgGalery/Image/grand-theft-auto-6-keyart.jpg"
+          />
+        </th>
+        <th>
+        Podle oficiálních informací by mělo Grand Theft Auto VI vyjít v roce 2025, přičemž šéf společnosti Ubisoft Yves Guillemot během telefonátu s investory (informuje o něm například portál <a target="_blank" href="https://www.ign.com/articles/gta-6-launch-will-also-be-positive-for-ubisoft-ceo-says">IGN</a>) odhadl vydání GTA VI nejdříve na duben 2025.
+        </th>
+      </tr>
     </table>
+    <table>
+    <tr class="ramecek">
+      <th>Kingdom Come: Deliverance 2 </th>
+      <th>
+        <img src="https://th.bing.com/th/id/OIF.gwuJVfntJ1ZqvhP4qgivsw?rs=1&pid=ImgDetMain">
+      </th>
+      <th>Český středověký hit Kingdom Come Deliverance dostane v roce 2024 pokračování, které nás opět v kůži Jindřicha ze Skalice zavede do Čech v 15. století</th>
+    </tr>
+    </table>  
     <script src="../javascript/js.js"></script>
   </body>
 </html>
